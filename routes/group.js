a5cec3457df48a8abb5d0d9d0dcd41ce4113f082
Valentine Veremeienko <valik372@gmail.com>
2020-03-05 16:48:30 +0200

    added groups (low functionality)

--- routes/group.js
+++ routes/group.js
@@ -1,27 +1,35 @@
 const {Router} = require('express')
 const Group = require('../models/group')
+const authMiddleware = require('../middleware/auth')
+const flash = require('connect-flash')
 
 const router = new Router()
 
-router.get('/', async (req, res) => {
+router.get('/', authMiddleware, async (req, res) => {
   res.render('groups', {
     title: "Groups management",
-    groups: await Group.find()
+    groups: await Group.find(),
+    isTeacher: req.session.isAuthenticatedTeacher,
+    isAdmin: req.session.isAdmin,
+    isAuth: req.session.isAuth,
   })
 })
 
-router.get('/edit/:id', async (req, res) => {
+router.get('/edit/:id', authMiddleware, async (req, res) => {
   try {
     res.render('editGroup', {
       title: "Edit current group",
-      group: await Group.findById(req.params.id)
+      group: await Group.findById(req.params.id),
+      isTeacher: req.session.isAuthenticatedTeacher,
+      isAdmin: req.session.isAdmin,
+      isAuth: req.session.isAuth,
     })
   } catch (e) {
     console.log(e);
   }
 })
 
-router.post('/create', async (req, res) => {
+router.post('/create', authMiddleware, async (req, res) => {
   try {
     const newGroup = new Group({name: req.body.groupName})
     await newGroup.save()
@@ -31,20 +39,25 @@ router.post('/create', async (req, res) => {
   }
 })
 
-router.post('/edit', async (req, res) => {
-  const {id} = req.body
+router.post('/edit', authMiddleware, async (req, res) => {
   try {
+    const {id} = req.body
+    if (!(await Group.findOne({name: req.body.name}))) {
       delete req.body.id
       const group = await Group.findById(id)
       Object.assign(group, req.body)
       await group.save()
-      res.redirect('/groups/edit/'+id)
+      req.flash('success', 'Group has been renamed...')
+    } else {
+      req.flash('error', 'Group with this name is already exist...')
+    }
+    res.redirect('/groups/edit/'+id)
   } catch(e) {
       console.log(e)
   }
 })
 
-router.post('/delete', async (req, res) => {
+router.post('/delete', authMiddleware, async (req, res) => {
   try {
     await Group.deleteOne({_id: req.body.id})
     res.redirect('groups')
