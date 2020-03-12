48694d27af7ea1a77f0ccc63fd9f997778c5de17
romaniy <roma.saybert@gmail.com>
2020-03-12 15:25:50 +0200

    added json response

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
-const {Router} = require('express')
-const {validationResult} = require('express-validator')
+const { Router } = require('express')
+const { validationResult } = require('express-validator')
 const User = require('../models/user')
-const {authMiddleware, authNotMiddleware} = require('../middleware/auth')
+const { authMiddleware, authNotMiddleware } = require('../middleware/auth')
 const bcrypt = require('bcryptjs')
 const flash = require('connect-flash')
-const {loginValidators, registerValidators, changePasswordValidators, resetValidators, setPasswordValidators} = require('../utils/validators')
+const { loginValidators, registerValidators, changePasswordValidators, resetValidators, setPasswordValidators } = require('../utils/validators')
 const crypto = require('crypto')
 const nodemailer = require('nodemailer')
 const sendgrid = require('nodemailer-sendgrid-transport')
@@ -14,47 +14,12 @@ const resEmail = require('../emails/reset')
 
 const router = new Router()
 const transporter = nodemailer.createTransport(sendgrid({
-    auth: {api_key: keys.SENDGRID_API_KEY}
+    auth: { api_key: keys.SENDGRID_API_KEY }
 }))
 
-router.get('/', authMiddleware , async (req,res)=> {
-    const user = await User.findById(req.session.userId)
-    let notifications = []
-    if (user.role === 'student') {
-        notifications = user.notification
-    }
-    res.render('index', {
-        title: 'Academy',
-        isTeacher: user.role === 'teacher',
-        isAdmin: user.role === 'admin',
-        isAuth: req.session.isAuth,
-        notifications
-    })
-    await User.findByIdAndUpdate(req.session.userId, {$unset: {notification: ''}})
-})
-
-router.get('/login', authNotMiddleware, async (req,res)=> {
-    res.render('auth/login', {
-        title: 'Login',
-        isAuth: req.session.isAuth
-    })
-})
-
-router.get('/register', authMiddleware, async (req,res)=> {
-    const userRole = (await User.findById(req.session.userId)).role
-    if (userRole === 'admin' || userRole === 'teacher') {
-        res.render('auth/register', {
-            title: userRole === 'admin' ? 'Add teacher' : 'Add student',
-            isTeacher: userRole === 'teacher',
-            isAdmin: userRole === 'admin',
-            isAuth: req.session.isAuth
-        })
-    }
-})
-
-router.get('/logout', authMiddleware, async (req, res) => {
+router.delete('/logout', authMiddleware, async (req, res) => {
     req.session.destroy(() => {
-        res.redirect('/login')
+        res.status(200).json("Session deleted...")
     })
 })
 
@@ -62,9 +27,9 @@ router.get('/password/:token', authNotMiddleware, async (req, res) => {
     try {
         if (!req.params.token) res.redirect('/login')
 
-        const user = await User.findOne({resetToken: req.params.token, resetTokenExp: {$gt: Date.now()}})
+        const user = await User.findOne({ resetToken: req.params.token, resetTokenExp: { $gt: Date.now() } })
         if (!user) {
-            return res.redirect('/login')
+            return res.status(404).json("User not found...")
         } else {
             res.render('auth/password', {
                 title: 'Update password',
@@ -77,10 +42,9 @@ router.get('/password/:token', authNotMiddleware, async (req, res) => {
     }
 })
 
-router.get('/profile', authMiddleware, async (req,res)=> {
+router.get('/profile', authMiddleware, async (req, res) => {
     const user = await User.findById(req.session.userId)
-    res.render('profile', {
-        title: 'My profile',
+    res.status(200).json({
         isTeacher: user.role === 'teacher',
         isAdmin: user.role === 'admin',
         isAuth: req.session.isAuth,
@@ -90,28 +54,22 @@ router.get('/profile', authMiddleware, async (req,res)=> {
     })
 })
 
-router.get('/reset', authNotMiddleware, (req, res) => {
-    res.render('auth/reset', {
-        title: 'Forgot password?'
-    })
-})
-
 router.patch('/reset', authNotMiddleware, resetValidators, (req, res) => {
     try {
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             req.flash('error', errors.array()[0].msg)
-            return res.status(422).redirect('/reset')
+            return res.status(422).json(`${errors.array()[0].msg}`)
         }
 
         crypto.randomBytes(32, async (err, buffer) => {
             if (err) {
                 req.flash('error', 'Oops! Something went wrong...')
-                return res.redirect('/reset')
+                return res.status(422).json("Oops! Something went wrong...")
             }
-            await User.findOneAndUpdate({email: req.body.email}, {resetToken: buffer.toString('hex'), resetTokenExp: Date.now() + 3600000})
+            const user = await User.findOneAndUpdate({ email: req.body.email }, { resetToken: buffer.toString('hex'), resetTokenExp: Date.now() + 3600000 })
             await transporter.sendMail(resEmail(req.body.email, buffer.toString('hex')))
-            res.redirect('/login')
+            res.status(200).json({user})
         })
     } catch (e) {
         console.log(e)
@@ -124,66 +82,68 @@ router.patch('/password', authNotMiddleware, setPasswordValidators, async (req, 
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             req.flash('error', errors.array()[0].msg)
-            return res.status(422).redirect('/password/' + req.body.token)
+            return res.status(422).json(`${errors.array()[0].msg}`)
         }
-        await User.findOneAndUpdate({
+        const user = await User.findOneAndUpdate({
             _id: req.body.userId,
             resetToken: req.body.token,
-            resetTokenExp: {$gt: Date.now()}
+            resetTokenExp: { $gt: Date.now() }
         }, {
-            password: await bcrypt.hash(req.body.password, 10), 
-            $unset: {resetToken: "", resetTokenExp: ""}
+            password: await bcrypt.hash(req.body.password, 10),
+            $unset: { resetToken: "", resetTokenExp: "" }
         })
-        res.redirect('/login')
+        res.status(200).json({user})
     } catch (e) {
         console.log(e)
     }
 })
 
-router.patch('/change_password', authMiddleware, changePasswordValidators, async (req,res)=> {
+router.patch('/change_password', authMiddleware, changePasswordValidators, async (req, res) => {
     try {
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             req.flash('error', errors.array()[0].msg)
-            return res.status(422).redirect('/profile')
+            return res.status(422).json(`${errors.array()[0].msg}`)
         }
         const oldUserPassword = (await User.findById(req.session.userId)).password
         if (await bcrypt.compare(req.body.oldPassword, oldUserPassword)) {
-            await User.findByIdAndUpdate(req.session.userId, {password: (await bcrypt.hash(req.body.newPassword, 10)).toString()})
+            const user = await User.findByIdAndUpdate(req.session.userId, { password: (await bcrypt.hash(req.body.newPassword, 10)).toString() })
             req.flash('success', 'Password has been changed...')
+            res.status(200).json({user})
         } else {
             req.flash('error', 'Invalid old password')
+            res.status(422).json("Invalid old password...")
         }
-        res.redirect('/profile') 
     } catch (e) {
         console.log(e)
     }
 })
 
-router.post('/login', loginValidators, async (req,res)=> {
+router.post('/login', loginValidators, async (req, res) => {
     try {
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             req.flash('error', errors.array()[0].msg)
-            return res.status(422).redirect('/login')
+            return res.status(422).json(`${errors.array()[0].msg}`)
         }
-        res.redirect('/')
+        const user = await User.findById(req.session.userId).select("name email _id role")
+        res.status(200).json({user})
     } catch (e) {
         console.log(e)
     }
 })
 
-router.post('/register', authMiddleware, registerValidators, async (req,res)=> {
+router.post('/register', authMiddleware, registerValidators, async (req, res) => {
     try {
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             req.flash('error', errors.array()[0].msg)
-            return res.status(422).redirect('/register')
+            return res.status(422).json(`${errors.array()[0].msg}`)
         }
 
-        const {username, password, email} = req.body
+        const { username, password, email } = req.body
         const userRole = (await User.findById(req.session.userId)).role
-        
+
         if (userRole === 'admin' || userRole === 'teacher') {
             const user = new User({
                 name: username,
@@ -193,8 +153,8 @@ router.post('/register', authMiddleware, registerValidators, async (req,res)=> {
             })
             await user.save()
             await transporter.sendMail(regEmail(username, email, password))
+            res.status(200).json({user})
         }
-        res.redirect('/')
     } catch (e) {
         console.log(e)
     }
--- routes/group.js
+++ routes/group.js
@@ -1,19 +1,18 @@
 const { Router } = require('express')
 const { validationResult } = require('express-validator')
 const Group = require('../models/group')
-const {authMiddleware} = require('../middleware/auth')
+const { authMiddleware } = require('../middleware/auth')
 const teacherMiddleware = require('../middleware/teacher')
 const flash = require('connect-flash')
 const ObjectId = require('mongodb').ObjectID
 const User = require('../models/user')
-const {groupIdValidator, addStudentValidator, notificationValidator, groupNameValidator, groupEditValidator, groupDeleteValidator, deleteStudentsValidators} = require('../utils/validators')
+const { groupIdValidator, addStudentValidator, notificationValidator, groupNameValidator, groupEditValidator, groupDeleteValidator, deleteStudentsValidators } = require('../utils/validators')
 
 const router = new Router()
 
 router.get('/', authMiddleware, async (req, res) => {
   const { role } = await User.findById(req.session.userId)
-  res.render('groups/show-reg-notify', {
-    title: (role === "teacher") ? "Groups management" : "Groups",
+  res.status(200).json({
     groups: await Group.find(),
     isTeacher: role === "teacher",
     isAdmin: role === "admin",
@@ -25,11 +24,9 @@ router.get('/:id', authMiddleware, groupIdValidator, async (req, res) => {
   try {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return res.status(422).redirect('/groups')
+      return res.status(422).json(`${errors.array()[0].msg}`)
     }
-
-    res.render('groups/edit', {
-      title: "Group",
+    res.status(200).json({
       group: await Group.findById(req.params.id),
       isTeacher: (await User.findById(req.session.userId)).role === "teacher",
       isAdmin: (await User.findById(req.session.userId)).role === "admin",
@@ -42,17 +39,16 @@ router.get('/:id', authMiddleware, groupIdValidator, async (req, res) => {
   }
 })
 
-router.post('/add_student', authMiddleware, teacherMiddleware, addStudentValidator, async (req, res) => {
+router.put('/add_student', authMiddleware, teacherMiddleware, addStudentValidator, async (req, res) => {
   try {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       req.flash('error', errors.array()[0].msg)
-      return res.status(422).redirect('/groups/' + req.body.groupId)
+      return res.status(422).json(`${errors.array()[0].msg}`)
     }
-
-    await Group.findByIdAndUpdate(req.body.groupId, { $push: { students: req.body.studentId } })
-    await User.findByIdAndUpdate(req.body.studentId, { group: req.body.groupId })
-    res.redirect('/groups/' + req.body.groupId)
+    const group = await Group.findByIdAndUpdate(req.body.groupId, { $push: { students: req.body.studentId } })
+    const user = await User.findByIdAndUpdate(req.body.studentId, { group: req.body.groupId })
+    res.status(200).json({ group, user })
   } catch (e) {
     console.log(e)
   }
@@ -62,12 +58,11 @@ router.post('/notification', authMiddleware, teacherMiddleware, notificationVali
   try {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return res.status(422).redirect('/groups#addNotification')
+      return res.status(422).json(`${errors.array()[0].msg}`)
     }
-
     const teacherName = (await User.findById(req.session.userId)).name
     await User.updateMany({ group: req.body.group }, { $push: { notification: { author: teacherName, message: req.body.message } } })
-    res.redirect('/groups#addNotification')
+    res.status(200).json({users: await User.find({group: req.body.group})})
   } catch (e) {
     console.log(e)
   }
@@ -78,41 +73,38 @@ router.post('/create', authMiddleware, teacherMiddleware, groupNameValidator, as
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       req.flash('error', errors.array()[0].msg)
-      return res.status(422).redirect('/groups#createGroup')
+      return res.status(422).json(`${errors.array()[0].msg}`)
     }
-
     const newGroup = new Group({ name: req.body.name })
     await newGroup.save()
-    res.redirect('/groups/' + newGroup._id)
+    res.status(200).json({newGroup})
   } catch (e) {
     console.log(e)
   }
 })
 
-router.patch('/edit', authMiddleware, teacherMiddleware, groupEditValidator, async (req, res) => {
+router.put('/edit', authMiddleware, teacherMiddleware, groupEditValidator, async (req, res) => {
   try {
-    const {id} = req.body
+    const { id } = req.body
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       req.flash('error', errors.array()[0].msg)
-      return res.status(422).redirect('/groups/' + id)
+      return res.status(422).json(`${errors.array()[0].msg}`)
     }
-
-    await Group.findByIdAndUpdate(id, { name: req.body.name })
+    const group = await Group.findByIdAndUpdate(id, { name: req.body.name })
     req.flash('success', 'Group has been renamed...')
-    res.redirect('/groups/' + id)
+    res.status(200).json({group})
   } catch (e) {
     console.log(e)
   }
 })
 
-router.patch('/delete_students', authMiddleware, teacherMiddleware, deleteStudentsValidators, async (req,res) => {
+router.put('/delete_students', authMiddleware, teacherMiddleware, deleteStudentsValidators, async (req, res) => {
   try {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return res.status(422).redirect('/groups/' + req.body.groupId)
+      return res.status(422).json(`${errors.array()[0].msg}`)
     }
-
     let students_id = (await Group.findById(req.body.groupId)).students
     let delete_students = []
     for (let i = students_id.length - 1; i >= 0; i--) {
@@ -121,9 +113,9 @@ router.patch('/delete_students', authMiddleware, teacherMiddleware, deleteStuden
       }
     }
     students_id = students_id.filter(st => !(delete_students.includes(st)))
-    await Group.findByIdAndUpdate(req.body.groupId, { students: students_id })
-    await User.updateMany({ _id: delete_students }, { $unset: { group: "" } })
-    res.redirect('/groups/' + req.body.groupId)
+    const group = await Group.findByIdAndUpdate(req.body.groupId, { students: students_id })
+    const user = await User.updateMany({ _id: delete_students }, { $unset: { group: "" } })
+    res.status(200).json({ group, user })
   } catch (e) {
     console.log(e)
   }
@@ -133,12 +125,11 @@ router.delete('/delete', authMiddleware, teacherMiddleware, groupDeleteValidator
   try {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return res.status(422).redirect('/groups')
+      return res.status(422).json(`${errors.array()[0].msg}`)
     }
-
     await User.updateMany({ group: req.body.id }, { $unset: { group: "" } })
-    await Group.findByIdAndDelete(req.body.id)
-    res.redirect('/groups')
+    const deleteGroup = await Group.findByIdAndDelete(req.body.id)
+    res.status(200).json({users: await User.find({group: req.body.id}), deleteGroup})
   } catch (e) {
     console.log(e)
   }
